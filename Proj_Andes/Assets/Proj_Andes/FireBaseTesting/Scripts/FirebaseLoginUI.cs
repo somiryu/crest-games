72051bbc46beb3e29a726b8a1c852a7d5f7b96d1
Yezael_Castillo <yezael.castillo@endlessadventuresinc.com>
2023-12-08 16:16:58 -0500

    Added a log in panel to the "FireBaseTestScene" You can now log in if you had a previously created account

--- Proj_Andes/Assets/Proj_Andes/FireBaseTesting/Scripts/FirebaseLoginUI.cs
+++ Proj_Andes/Assets/Proj_Andes/FireBaseTesting/Scripts/FirebaseLoginUI.cs
@@ -8,15 +8,39 @@ using UnityEngine.UI;
 public class FirebaseLoginUI : MonoBehaviour
 {
 
+	[Header("RegisterPanel")]
+	public GameObject registerNewPanel;
 	public TMP_InputField emailField;
 	public TMP_InputField passwordField;
+	public Button RegisterBtn;
+	public Button switchToLogIn;
 
-	public Button enterBtn;
+	[Header("Log in Panel")]
+	public GameObject logInPanel;
+	public TMP_InputField LogemailField;
+	public TMP_InputField LogpasswordField;
+	public Button LogInBtn;
+	public Button switchToRegister;
 
 
 	private void Awake()
 	{
-		enterBtn.onClick.AddListener(() => CreateNewUser(emailField.text, passwordField.text));
+		RegisterBtn.onClick.AddListener(() => CreateNewUser(emailField.text, passwordField.text));
+		LogInBtn.onClick.AddListener(() => LogInUser(LogemailField.text, LogpasswordField.text));
+		switchToLogIn.onClick.AddListener(OpenLogInPanel);
+		switchToRegister.onClick.AddListener(OpenRegisterPanel);
+	}
+
+	public void OpenLogInPanel()
+	{
+		registerNewPanel.SetActive(false);
+		logInPanel.SetActive(true);
+	}
+
+	public void OpenRegisterPanel()
+	{
+		registerNewPanel.SetActive(true);
+		logInPanel.SetActive(false);
 	}
 
 	public void CreateNewUser(string email, string password)
@@ -37,6 +61,27 @@ public class FirebaseLoginUI : MonoBehaviour
 			// Firebase user has been created.
 			Firebase.Auth.AuthResult result = task.Result;
 			Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+				result.User.DisplayName, result.User.UserId);
+		});
+	}
+
+	public void LogInUser(string email, string password)
+	{
+		FirebaseAuth FirebaseAuth = FirebaseAuth.DefaultInstance;
+		FirebaseAuth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+			if (task.IsCanceled)
+			{
+				Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+				return;
+			}
+			if (task.IsFaulted)
+			{
+				Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				return;
+			}
+
+			AuthResult result = task.Result;
+			Debug.LogFormat("User signed in successfully: {0} ({1})",
 				result.User.DisplayName, result.User.UserId);
 		});
 	}
