21fe9b3bf6ceb93d7f77e0b233571245f48a4daf
Yezael_Castillo <yezael.castillo@endlessadventuresinc.com>
2023-12-11 17:49:31 -0500

    - Done most basic UserData management - Done fields autofill for e mail and passwords to last used

--- Proj_Andes/Assets/Proj_Andes/FireBaseTesting/Scripts/FirebaseLoginUI.cs
+++ Proj_Andes/Assets/Proj_Andes/FireBaseTesting/Scripts/FirebaseLoginUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Firebase.Auth;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class FirebaseLoginUI : MonoBehaviour
 {
@@ -21,14 +22,81 @@ public class FirebaseLoginUI : MonoBehaviour
 	public TMP_InputField LogpasswordField;
 	public Button LogInBtn;
 	public Button switchToRegister;
+	public Toggle rememberPasswordToggle;
 
+	private string emailPrefID = "Email";
+	private string rememberPasswordPrefID = "RememberPassword";
+	private string passwordPrefID = "password";
+
+	bool onCorrectlyLoggegFlag = false;
+	bool logFailedFlag = false;
+	string currLogUserID = "";
+
+	IEnumerator currLogConfirmRoutine;
 
 	private void Awake()
 	{
-		RegisterBtn.onClick.AddListener(() => CreateNewUser(emailField.text, passwordField.text));
-		LogInBtn.onClick.AddListener(() => LogInUser(LogemailField.text, LogpasswordField.text));
+		RegisterBtn.onClick.AddListener(OnRegisterBtnPressed);
+		LogInBtn.onClick.AddListener(OnLogInBtnPressed);
 		switchToLogIn.onClick.AddListener(OpenLogInPanel);
 		switchToRegister.onClick.AddListener(OpenRegisterPanel);
+
+		rememberPasswordToggle.onValueChanged.AddListener(
+			(state) =>
+			{
+				if (rememberPasswordToggle.isOn)
+				{
+					PlayerPrefs.SetInt(rememberPasswordPrefID, 1);
+				}
+				else
+				{
+					PlayerPrefs.SetString(passwordPrefID, "");
+					PlayerPrefs.SetInt(rememberPasswordPrefID, -1);
+				}
+			}
+			);
+
+		UserDataManager.Init();
+
+		onCorrectlyLoggegFlag = false;
+		logFailedFlag = false;
+		currLogUserID = "";
+
+		var savedEmail = PlayerPrefs.GetString(emailPrefID);
+		if (!string.IsNullOrEmpty(savedEmail))
+		{
+			LogemailField.SetTextWithoutNotify(savedEmail);
+		}
+		var savePassword = PlayerPrefs.GetInt(rememberPasswordPrefID) == 1;
+		if (savePassword)
+		{
+			var passwordSaved = PlayerPrefs.GetString(passwordPrefID);
+			if (!string.IsNullOrEmpty(passwordSaved))
+			{
+				LogpasswordField.SetTextWithoutNotify(passwordSaved);
+			}
+		}
+		rememberPasswordToggle.isOn = savePassword;
+	}
+
+	void OnLogInBtnPressed()
+	{
+		LogInUser(LogemailField.text, LogpasswordField.text);
+		if (currLogConfirmRoutine == null)
+		{
+			currLogConfirmRoutine = CorrectlyLoggedRoutine();
+			StartCoroutine(currLogConfirmRoutine);
+		}
+	}
+
+	void OnRegisterBtnPressed()
+	{
+		CreateNewUser(emailField.text, passwordField.text);
+		if (currLogConfirmRoutine == null)
+		{
+			currLogConfirmRoutine = CorrectlyLoggedRoutine();
+			StartCoroutine(currLogConfirmRoutine);
+		}
 	}
 
 	public void OpenLogInPanel()
@@ -44,17 +112,19 @@ public class FirebaseLoginUI : MonoBehaviour
 	}
 
 	public void CreateNewUser(string email, string password)
-    {
+	{
 		FirebaseAuth FirebaseAuth = FirebaseAuth.DefaultInstance;
 		FirebaseAuth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
 			if (task.IsCanceled)
 			{
 				Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+				logFailedFlag = true;
 				return;
 			}
 			if (task.IsFaulted)
 			{
 				Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				logFailedFlag = true;
 				return;
 			}
 
@@ -62,6 +132,8 @@ public class FirebaseLoginUI : MonoBehaviour
 			Firebase.Auth.AuthResult result = task.Result;
 			Debug.LogFormat("Firebase user created successfully: {0} ({1})",
 				result.User.DisplayName, result.User.UserId);
+
+			onCorrectlyLoggegFlag = true;
 		});
 	}
 
@@ -72,17 +144,44 @@ public class FirebaseLoginUI : MonoBehaviour
 			if (task.IsCanceled)
 			{
 				Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+				logFailedFlag = true;
 				return;
 			}
 			if (task.IsFaulted)
 			{
 				Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				logFailedFlag = true;
 				return;
 			}
 
 			AuthResult result = task.Result;
 			Debug.LogFormat("User signed in successfully: {0} ({1})",
 				result.User.DisplayName, result.User.UserId);
+
+			onCorrectlyLoggegFlag = true;
+			currLogUserID = result.User.UserId;
 		});
+	}
+
+	void OnCorrectlyLogIn(string email, string ID)
+	{
+		PlayerPrefs.SetString(emailPrefID, LogemailField.text);
+		if (rememberPasswordToggle.isOn)
+		{
+			PlayerPrefs.SetString(passwordPrefID, LogpasswordField.text);
+		}
+		PlayerPrefs.Save();
+		UserDataManager.Instance.SetCurrUser(email, ID);
+	}
+
+	IEnumerator CorrectlyLoggedRoutine()
+	{
+		while (!onCorrectlyLoggegFlag && !logFailedFlag) yield return null;
+
+		if (onCorrectlyLoggegFlag) OnCorrectlyLogIn(LogemailField.text, currLogUserID);
+
+		logFailedFlag = false;
+		onCorrectlyLoggegFlag = false;
+		currLogConfirmRoutine = null;
 	}
 }
