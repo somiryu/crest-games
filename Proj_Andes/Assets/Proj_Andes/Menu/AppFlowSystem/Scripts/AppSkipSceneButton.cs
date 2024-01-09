3e0bbe8e762f9f38b595e21e81631d0ebe0c6e1a
Yezael_Castillo <yezael.castillo@endlessadventuresinc.com>
2024-01-09 08:46:26 -0500

    Updated folder structure

--- Proj_Andes/Assets/Proj_Andes/Menu/AppFlowSystem/Scripts/AppSkipSceneButton.cs
+++ Proj_Andes/Assets/Proj_Andes/Menu/AppFlowSystem/Scripts/AppSkipSceneButton.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings.SplashScreen;
 
 public class AppSkipSceneButton : MonoBehaviour
 {
