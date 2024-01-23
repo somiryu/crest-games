124d22daf89dec98be1ae1a79f14c227f0950503
Aurora Rodríguez <91167758+RoraAu18@users.noreply.github.com>
2024-01-23 15:25:43 -0500

    boosters and scape analytics added

--- Proj_Andes/Assets/Proj_Andes/UserData/Resources/DataIds.cs
+++ Proj_Andes/Assets/Proj_Andes/UserData/Resources/DataIds.cs
@@ -8,7 +8,9 @@ public static class DataIds
     public const string voiceStartimeToMakeAChoice = "voiceStartimeToMakeAChoice";
     public const string voiceStarRoundResultWins = "voiceStarRoundResultWins";
 
-    
-
+    public const string boostersAndScapetotalAttemptsToBoost = "boostersAndScapetotalAttemptsToBoost";
+    public const string boostersAndScapeTotalGametime = "boostersAndScapeTotalGametime";
+    public const string boostersAndScapetimeToMakeAChoice = "boostersAndScapetimeToMakeAChoice";
+    public const string boostersAndScapeRoundResultWins = "boostersAndScapeRoundResultWins";
 
 }
